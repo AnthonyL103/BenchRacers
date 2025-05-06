eee0644305db5a02d1d9417ecad6a6c7d70eff01
AnthonyL103 <143826886+AnthonyL103@users.noreply.github.com>
2025-05-05 19:30:20 -0700

    added data reads for rds and amplify

--- main.tf
+++ main.tf
@@ -13,6 +13,14 @@ provider "aws" {
 
 data "aws_region" "current" {}
 
+data "aws_db_instance" "benchracers_rds" {
+  db_instance_identifier = "database-1"
+}
+
+data "aws_amplify_app" "benchracers_amplify" {
+  app_id = "d3bmjlq8fmb7xr"
+}
+
 resource "aws_cloudwatch_log_group" "benchracers_api_logs" {
   name              = "/benchracers/api/calls"
   retention_in_days = 14
@@ -139,6 +147,36 @@ resource "aws_security_group" "benchracers_alb_sg" {
 
   tags = { Name = "BenchRacers-ALB-SG" }
 }
+
+resource "aws_security_group" "benchracers_ec2_sg" {
+  name        = "benchracers-ec2-sg"
+  description = "Security group for BenchRacers EC2 instances"
+  vpc_id      = aws_vpc.benchracers_vpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.benchracers_alb_sg.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "BenchRacers-EC2-SG" }
+}
+
 
 resource "aws_security_group" "benchracers_ec2_sg" {
   name        = "benchracers-ec2-sg"
