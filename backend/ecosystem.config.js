3f6945ec728ec5c9b9595072ac2a72f14b7efe3f
AnthonyL103 <143826886+AnthonyL103@users.noreply.github.com>
2025-05-06 12:58:13 -0700

    Update ecosystem config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "benchracers-backend",
-        script: "./dist/server.js", // after compiling with tsc
+        script: "./dist/server.ts", // after compiling with tsc
         instances: 1,
         env: {
           NODE_ENV: "production",
